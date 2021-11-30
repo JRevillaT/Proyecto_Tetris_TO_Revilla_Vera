347728805702ec7e572b7f886e5aaa52138d6f42
JoseMiguelVM <64231356+JoseMiguelVM@users.noreply.github.com>
2021-11-30 18:51:44 -0500

    Actualizando variables

--- Proyecto/TetrixWindow.cpp
+++ Proyecto/TetrixWindow.cpp
@@ -18,38 +18,38 @@
 TetrixWindow::TetrixWindow(QWidget *parent)
     :QWidget(parent), tablero(new TetrixBoard){
 //! [0]
-    nextPieceLabel = new QLabel;
-    nextPieceLabel->setFrameStyle(QFrame::Box | QFrame::Raised);
-    nextPieceLabel->setAlignment(Qt::AlignCenter);
-    tablero->setNextPieceLabel(nextPieceLabel);
+    siguientePieza = new QLabel;
+    siguientePieza->setFrameStyle(QFrame::Box | QFrame::Raised);
+    siguientePieza->setAlignment(Qt::AlignCenter);
+    tablero->setNextPieceLabel(siguientePieza);
 //! [1]
-    scoreLcd = new QLCDNumber(5);
-    scoreLcd->setSegmentStyle(QLCDNumber::Filled);
+    puntuacion = new QLCDNumber(5);
+    puntuacion->setSegmentStyle(QLCDNumber::Filled);
 //! [1]
-    levelLcd = new QLCDNumber(2);
-    levelLcd->setSegmentStyle(QLCDNumber::Filled);
+    nivel = new QLCDNumber(2);
+    nivel->setSegmentStyle(QLCDNumber::Filled);
     linesLcd = new QLCDNumber(5);
     linesLcd->setSegmentStyle(QLCDNumber::Filled);
 
 //! [2]
-    startButton = new QPushButton(tr("&Start"));
-    startButton->setFocusPolicy(Qt::NoFocus);
-    quitButton = new QPushButton(tr("&Quit"));
-    quitButton->setFocusPolicy(Qt::NoFocus);
-    pauseButton = new QPushButton(tr("&Pause"));
+    botonIniciar = new QPushButton(tr("&Start"));
+    botonIniciar->setFocusPolicy(Qt::NoFocus);
+    botonSalir = new QPushButton(tr("&Quit"));
+    botonSalir->setFocusPolicy(Qt::NoFocus);
+    botonPausar = new QPushButton(tr("&Pause"));
 //! [2] //! [3]
-    pauseButton->setFocusPolicy(Qt::NoFocus);
+    botonPausar->setFocusPolicy(Qt::NoFocus);
 //! [3] //! [4]
 
-    connect(startButton, &QPushButton::clicked, tablero, &TetrixBoard::start);
+    connect(botonIniciar, &QPushButton::clicked, tablero, &TetrixBoard::start);
 //! [4] //! [5]
-    connect(quitButton , &QPushButton::clicked, qApp, &QCoreApplication::quit);
-    connect(pauseButton, &QPushButton::clicked, tablero, &TetrixBoard::pause);
+    connect(botonSalir , &QPushButton::clicked, qApp, &QCoreApplication::quit);
+    connect(botonPausar, &QPushButton::clicked, tablero, &TetrixBoard::pause);
 #if __cplusplus >= 201402L
     connect(tablero, &TetrixBoard::scoreChanged,
-            scoreLcd, qOverload<int>(&QLCDNumber::display));
+            puntuacion, qOverload<int>(&QLCDNumber::display));
     connect(tablero, &TetrixBoard::levelChanged,
-            levelLcd, qOverload<int>(&QLCDNumber::display));
+            nivel, qOverload<int>(&QLCDNumber::display));
     connect(tablero, &TetrixBoard::linesRemovedChanged,
             linesLcd, qOverload<int>(&QLCDNumber::display));
 #else
@@ -65,17 +65,17 @@ TetrixWindow::TetrixWindow(QWidget *parent)
 //! [6]
     QGridLayout *layout = new QGridLayout;
     layout->addWidget(createLabel(tr("NEXT")), 0, 0);
-    layout->addWidget(nextPieceLabel, 1, 0);
+    layout->addWidget(siguientePieza, 1, 0);
     layout->addWidget(createLabel(tr("LEVEL")), 2, 0);
-    layout->addWidget(levelLcd, 3, 0);
-    layout->addWidget(startButton, 4, 0);
+    layout->addWidget(nivel, 3, 0);
+    layout->addWidget(botonIniciar, 4, 0);
     layout->addWidget(tablero, 0, 1, 6, 1);
     layout->addWidget(createLabel(tr("SCORE")), 0, 2);
-    layout->addWidget(scoreLcd, 1, 2);
+    layout->addWidget(puntuacion, 1, 2);
     layout->addWidget(createLabel(tr("LINES REMOVED")), 2, 2);
     layout->addWidget(linesLcd, 3, 2);
-    layout->addWidget(quitButton, 4, 2);
-    layout->addWidget(pauseButton, 5, 2);
+    layout->addWidget(botonSalir, 4, 2);
+    layout->addWidget(botonPausar, 5, 2);
     setLayout(layout);
 
     setWindowTitle(tr("Tetrix"));
