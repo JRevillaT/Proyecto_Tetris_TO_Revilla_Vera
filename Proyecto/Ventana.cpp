aad19b779338b9add5f189e3c74aaa322ecbd3b8
JoseMiguelVM <64231356+JoseMiguelVM@users.noreply.github.com>
2021-11-30 19:20:20 -0500

    Eliminacion de comentarios y cambio de nombre de variables

--- Proyecto/Ventana.cpp
+++ Proyecto/Ventana.cpp
@@ -1,5 +1,5 @@
 #include "TetrixBoard.h"
-#include "tetrixwindow.h"
+#include "Ventana.h"
 
 #include <QCoreApplication>
 #include <QGridLayout>
@@ -15,34 +15,34 @@
  *  Repositorio: https://github.com/JRevillaT/Proyecto_Tetris_TO_Revilla_Vera
  */
 
-TetrixWindow::TetrixWindow(QWidget *parent)
+TWindow::TWindow(QWidget *parent)
     :QWidget(parent), tablero(new TetrixBoard){
-//! [0]
+
     siguientePieza = new QLabel;
     siguientePieza->setFrameStyle(QFrame::Box | QFrame::Raised);
     siguientePieza->setAlignment(Qt::AlignCenter);
     tablero->setNextPieceLabel(siguientePieza);
-//! [1]
+
     puntuacion = new QLCDNumber(5);
     puntuacion->setSegmentStyle(QLCDNumber::Filled);
-//! [1]
+
     nivel = new QLCDNumber(2);
     nivel->setSegmentStyle(QLCDNumber::Filled);
     linesLcd = new QLCDNumber(5);
     linesLcd->setSegmentStyle(QLCDNumber::Filled);
 
-//! [2]
+
     botonIniciar = new QPushButton(tr("&Start"));
     botonIniciar->setFocusPolicy(Qt::NoFocus);
     botonSalir = new QPushButton(tr("&Quit"));
     botonSalir->setFocusPolicy(Qt::NoFocus);
     botonPausar = new QPushButton(tr("&Pause"));
-//! [2] //! [3]
+
     botonPausar->setFocusPolicy(Qt::NoFocus);
-//! [3] //! [4]
+
 
     connect(botonIniciar, &QPushButton::clicked, tablero, &TetrixBoard::start);
-//! [4] //! [5]
+
     connect(botonSalir , &QPushButton::clicked, qApp, &QCoreApplication::quit);
     connect(botonPausar, &QPushButton::clicked, tablero, &TetrixBoard::pause);
 #if __cplusplus >= 201402L
@@ -60,19 +60,17 @@ TetrixWindow::TetrixWindow(QWidget *parent)
     connect(board, &TetrixBoard::linesRemovedChanged,
             linesLcd, QOverload<int>::of(&QLCDNumber::display));
 #endif
-//! [5]
 
-//! [6]
     QGridLayout *layout = new QGridLayout;
-    layout->addWidget(createLabel(tr("NEXT")), 0, 0);
+    layout->addWidget(crearLabel(tr("NEXT")), 0, 0);
     layout->addWidget(siguientePieza, 1, 0);
-    layout->addWidget(createLabel(tr("LEVEL")), 2, 0);
+    layout->addWidget(crearLabel(tr("LEVEL")), 2, 0);
     layout->addWidget(nivel, 3, 0);
     layout->addWidget(botonIniciar, 4, 0);
     layout->addWidget(tablero, 0, 1, 6, 1);
-    layout->addWidget(createLabel(tr("SCORE")), 0, 2);
+    layout->addWidget(crearLabel(tr("SCORE")), 0, 2);
     layout->addWidget(puntuacion, 1, 2);
-    layout->addWidget(createLabel(tr("LINES REMOVED")), 2, 2);
+    layout->addWidget(crearLabel(tr("LINES REMOVED")), 2, 2);
     layout->addWidget(linesLcd, 3, 2);
     layout->addWidget(botonSalir, 4, 2);
     layout->addWidget(botonPausar, 5, 2);
@@ -81,12 +79,10 @@ TetrixWindow::TetrixWindow(QWidget *parent)
     setWindowTitle(tr("Tetrix"));
     resize(550, 370);
 }
-//! [6]
 
-//! [7]
-QLabel *TetrixWindow::createLabel(const QString &text){
+QLabel *TWindow::crearLabel(const QString &text){
     QLabel *label = new QLabel(text);
     label->setAlignment(Qt::AlignHCenter | Qt::AlignBottom);
     return label;
 }
-//! [7]
+
